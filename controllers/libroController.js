c7ac70b3afe216f78598f9fb26a405eff3f8029f
Eduardo-1921 <manuel@gmail.com>
2025-02-20 18:05:14 -0500

    2

--- controllers/libroController.js
+++ controllers/libroController.js
@@ -1,6 +1,21 @@
 const db = require("../config/database");
 
 class LibroController {
+    
+    static async obtenerLibros(req, res) {
+        let [lista] = await db.query("SELECT * FROM libros");
+        res.json(lista);
+    }
+
+    static async insertarLibro(req, res) {
+        let { titulo, autor, anio_publicacion, disponible } = req.body;
+        let libro = await db.query(
+            "INSERT INTO libros (titulo, autor, anio_publicacion, disponible) VALUES (?, ?, ?, ?)", 
+            [titulo, autor, anio_publicacion, disponible || true]
+        );
+        res.json(req.body);
+    }
 }
 
 module.exports = LibroController;
+
